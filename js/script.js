8f8876b5ba5c0093eeb776e4be318ec26df6952b
Miguel Mendez <michaelmndz1141@gmail.com>
2021-02-08 20:53:12 -0700

    Added basic functionality
    Now able to rotate through the quotes

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
  * `quotes` array 
 ***/
@@ -16,47 +12,72 @@ const quotes = [
   {
     quote: "Make things as simple as possible, but not simpler.",
     source: "Albert Einstein",
-    citation: "",
-    year: ""
+    year: "9999"
   },
   {
     quote: "If you cant explain it simply, you dont understand it well enough.",
     source: "Albert Einstein",
-    citation: "",
-    year: ""
   },
   {
     quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
     source: "Mahatma Gandhi",
-    citation: "",
-    year: ""
   },
   {
     quote: "Your time is limited so don't waste it living someone else's life.",
     source: "Steve Jobs",
-    citation: "",
-    year: ""
   },
   {
     quote: "When you realize you want to spend the rest of your life with somebody, you want the rest of your life to start as soon as possible.",
-    source: "Steve Jobs",
+    source: "Harry Burns",
     citation: "When Harry Met Sally",
     year: "1989"
   },
 ];
 
 
-
 /***
  * `getRandomQuote` function
 ***/
-
-
-
+// Accepts an array, returns a random object from the array
+function getRandomQuote(arr) {
+  const randomNumber = Math.floor(Math.random() * arr.length) 
+  return arr[randomNumber];
+}
 /***
  * `printQuote` function
 ***/
+// Uses getRandomQoute function to store the random object 
+function printQuote() {
+  const quoteObject = getRandomQuote(quotes);
+  const quote = quoteObject.quote;
+  const source = quoteObject.source
+  const citation = quoteObject.citation;
+  const year = quoteObject.year;
 
+  let quoteHTML = `
+    <p class="quote">"${quote}"</p>
+    <p class="source">${source}
+  `;
+
+  // If the property 'citation' is in the object concatnate it to the quoteHTMl and close the p element from quoteHTML
+  if ( 'citation' in quoteObject && 'year' in quoteObject){
+    quoteHTML += `
+    <span class="citation">${citation}</span><span class="year">${year}</span></p>
+    `;
+  } else if ('citation' in quoteObject) {
+    quoteHTML += `
+    <span class="citation">${citation}</span></p>
+    `;
+  } else if ('year' in quoteObject) {
+    quoteHTML += `
+    <span class="year">${year}</span></p>`
+    ;
+  } else {
+    quoteHTML += `</p>`
+  }
+
+  return document.getElementById('quote-box').innerHTML = quoteHTML; 
+}
 
 
 /***
